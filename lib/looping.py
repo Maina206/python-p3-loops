988df706701c4ac5b511821f59806aaae3876689
Alexander <maina206mwangi@gmail.com>
2024-11-30 16:55:16 +0300

    conditional completed

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
+    i = 10
+    while i > 0:
+        print(i)
+        i-=1
+    print('Happy New Year!')
     pass
 
 def square_integers(int_list):
     # code goes here!
+    init_list = [num ** 2 for num in int_list]
+    return init_list
     pass
 
 def fizzbuzz():
     # code goes here!
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
